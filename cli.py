e6f3cf345e4c075aa41f57a8595a88ab5cab812a
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-04-14 03:09:11 +0300

    IMS-4 CLI interface/ Frontend

--- cli.py
+++ cli.py
@@ -3,23 +3,52 @@ import requests
 BASE = "http://127.0.0.1:5000"
 
 def view_items():
-    print(requests.get(f"{BASE}/items").json())
+    try:
+        res = requests.get(f"{BASE}/items")
+
+        if res.status_code == 200:
+            print(res.json())
+        else:
+            print("Error:", res.status_code, res.text)
+
+    except Exception as e:
+        print("Server not running or error:", e)
+
 
 def add_item():
-    data = {
+    item = {
         "id": int(input("ID: ")),
         "name": input("Name: "),
-        "quantity": int(input("Qty: "))
+        "quantity": int(input("Quantity: "))
     }
-    print(requests.post(f"{BASE}/items", json=data).json())
+
+    res = requests.post(f"{BASE}/items", json=item)
+    print(res.json())
+
+
+def delete_item():
+    item_id = input("ID to delete: ")
+    res = requests.delete(f"{BASE}/items/{item_id}")
+    print(res.json())
+
+
+def external():
+    barcode = input("Barcode: ")
+    res = requests.get(f"{BASE}/external/{barcode}")
+    print(res.json())
+
 
 while True:
-    print("\n1.View 2.Add 3.Exit")
+    print("\n1.View 2.Add 3.Delete 4.External 5.Exit")
     choice = input("> ")
 
     if choice == "1":
         view_items()
     elif choice == "2":
         add_item()
+    elif choice == "3":
+        delete_item()
+    elif choice == "4":
+        external()
     else:
         break
